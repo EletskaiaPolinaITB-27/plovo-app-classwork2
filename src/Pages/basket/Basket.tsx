760d6162118aa6c8740de8b405eecb868e11fcf3
Polina Eletskaia <polina.e@compsscollege.art>
2026-03-28 10:57:54 +0600

    basket

--- src/Pages/basket/Basket.tsx
+++ src/Pages/basket/Basket.tsx
@@ -1,13 +1,37 @@
 import { Container, Typography } from "@mui/material"
-import type { IBasketState } from "../../types"
+import type { IBasketState, IDeliveryDetails, IDish } from "../../types"
 import { Link } from "@mui/icons-material"
+import { useState } from "react"
+import { axiosApi } from "../../axiosApi"
 
 interface Props {
-    basketState: IBasketState
+    basketState: IBasketState,
+    addDishToBasket: (dish: IDish) => void,
+    decreaseDishInBasket: (dishId: string) => void,
+    clearBasket: () => void,
 }
 
-export const Basket = ({basketState}:Props) => {
+export const Basket = ({basketState,addDishToBasket, decreaseDishInBasket, clearBasket,}:Props) => {
     const {items,totalCount,totalPrice} = basketState
+    const [loading, setLoading] = useState(false);
+
+
+    const submitOrder = async (deliveryDetails: IDeliveryDetails) => {
+    setLoading(true);
+    try {
+      const orderData = {
+        items,
+        totalCount,
+        totalPrice,
+        deliveryDetails,
+      };
+
+      await axiosApi.post('/orders.json', orderData);
+      clearBasket();
+    } finally {
+      setLoading(false);
+    }
+  };
 
 
     if (items.length === 0){
@@ -27,8 +51,6 @@ export const Basket = ({basketState}:Props) => {
 
 
     return(
-        <div>
-            basket
-        </div>
+        <div></div>
     )
 }
